4c3287eacc3169aaf4b662a851077af0bdbd3ac2
The Kapitan. <159403179+chrisb9085@users.noreply.github.com>
2026-06-14 14:06:35 -0400

    Broadcast function and handle_conn

--- server.py
+++ server.py
@@ -4,21 +4,43 @@ import threading
 import sys # Needed to read port numbers from command line
 
 clients_lock = threading.Lock() # Mutex
-clients = {}
+clients = {} # client : username
 
 HOST = "127.0.0.1"
 PORT = int(sys.argv[1])
-PORT = "3400"
+
 
 if PORT < 1024:
     print("Must input a port number greater than 1023.")
     exit(0)
 
-def send_response(sock, status, lines=None):
-    msg = str(status) + "\n\n"
-    if lines:
-        msg += "\n".join(str(l) for l in lines) + "\n"
-    try:
-        sock.sendall(msg.encode())
-    except Exception:
-        pass
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+def broadcast(message):
+    for client in clients.keys:
+        client.send(message)
+
+def handle_conn(client):
+    while True:
+        try:
+            message = client.recv(1024) # 1 kb of data
+            broadcast(message)
+        except:
+            clients.pop(client)
+
+# Commands:
+# connect <ip> <port>
+# login <username>
+# who
+# broadcast <msg>
+# private <username> <msg>
+# quit
+
+# Response format:
+# Status Code
+# <EMPTY LINE>
+# Data section if required
+
