1ef905ae7e99c4fa443b61af3a2fb13b07db5b22
The Kapitan. <159403179+chrisb9085@users.noreply.github.com>
2026-06-14 13:29:41 -0400

    Command line input

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ import sys # Needed to read port numbers from command line
 clients_lock = threading.Lock() # Mutex
 clients = {}
 
+HOST = "127.0.0.1"
+PORT = int(sys.argv[1])
+PORT = "3400"
+
+if PORT < 1024:
+    print("Must input a port number greater than 1023.")
+    exit(0)
 
 def send_response(sock, status, lines=None):
     msg = str(status) + "\n\n"
